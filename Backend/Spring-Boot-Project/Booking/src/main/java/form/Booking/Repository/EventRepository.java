5a1c97351ee724c97bd174a9fb1a3e9e0587b40a
sachin <124781801+sachin20041@users.noreply.github.com>
2024-02-29 10:49:08 +0530

    Delete Backend directory

--- Backend/Spring-Boot-Project/Booking/src/main/java/form/Booking/Repository/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package form.Booking.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import form.Booking.model.Event;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
-
